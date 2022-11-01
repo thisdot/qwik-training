06661a4c66b4c4e6fcf97a77a0aca67b580e9417
JiaLiPassion <JiaLi.Passion@gmail.com>
2022-11-01 21:03:02 +0900

    module2: component binding expression prepare

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,8 +2,29 @@ import type { DocumentHead } from '@builder.io/qwik-city'
 import { component$ } from '@builder.io/qwik'
 
 export default component$(() => {
-  return <span>{'__put_something_here__'}</span>
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const data = {
+    name: 'Qwik',
+    description: DESCRIPTION,
+  }
+
+  return (
+    <>
+      <input value="data.name should go here" />
+      <br />
+      <textarea rows={10} cols={60}>
+        data.description should go here
+      </textarea>
+    </>
+  )
 })
+
+export const DESCRIPTION = `
+Qwik is designed for the fastest possible page load time,
+by delivering pure HTML with near 0 JavaScript for your
+pages to become interactive, regardless of how complex
+your site or app is. It achieves this via resumability
+of code.`
 
 export const head: DocumentHead = {
   title: 'Welcome to Qwik',
