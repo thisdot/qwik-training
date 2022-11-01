2eb57647949876c0d02ce15b6b1717f14cc5ebd7
JiaLiPassion <JiaLi.Passion@gmail.com>
2022-11-01 21:19:23 +0900

    module2: component event listener

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,22 +1,8 @@
 import type { DocumentHead } from '@builder.io/qwik-city'
 import { component$ } from '@builder.io/qwik'
 
-interface ItemProps {
-  message: string
-}
-
 export default component$(() => {
-  const message = `Insert Greeter component here. By composing components together large applications can be
-      written without putting all of the code into a single file/component.`
-  return (
-    <div>
-      <Greeter message={message} />
-    </div>
-  )
-})
-
-export const Greeter = component$((props: ItemProps) => {
-  return <div>{props.message}</div>
+  return <button>Click Me</button>
 })
 
 export const head: DocumentHead = {
