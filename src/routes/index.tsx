05af34879a09ce3efdc9d7e8838094f3c680e1fc
JiaLiPassion <JiaLi.Passion@gmail.com>
2022-11-03 21:01:10 +0900

    module3-advanced-component-exercise-2

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,17 +1,11 @@
 import type { DocumentHead } from '@builder.io/qwik-city'
-import { component$, useStore } from '@builder.io/qwik'
+import { component$ } from '@builder.io/qwik'
 
 export default component$(() => {
-  const store = useStore({ x: 0, y: 0 })
   return (
-    <div
-      document:onMouseMove$={(event) => {
-        store.x = event.clientX
-        store.y = event.clientY
-      }}
-    >
-      Your mouse location is ({store.x}, {store.y}).
-    </div>
+    <a href="/" onClick$={() => alert('do something else.')}>
+      click me!
+    </a>
   )
 })
 
