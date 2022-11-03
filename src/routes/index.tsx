bf4cde6a6f4642fb163d15648c1239b79bfbc806
JiaLiPassion <JiaLi.Passion@gmail.com>
2022-11-03 21:01:46 +0900

    module3-advanced-component-exercise-2-answer

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,7 +3,11 @@ import { component$ } from '@builder.io/qwik'
 
 export default component$(() => {
   return (
-    <a href="/" onClick$={() => alert('do something else.')}>
+    <a
+      href="/"
+      preventdefault:click
+      onClick$={() => alert('do something else.')}
+    >
       click me!
     </a>
   )
