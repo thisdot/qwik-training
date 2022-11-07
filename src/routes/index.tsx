ac178ad6adc4c69c8e21c66c94a66803724c3455
JiaLiPassion <JiaLi.Passion@gmail.com>
2022-11-07 22:45:21 +0900

    module4-state-exercise-5

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,40 +1,68 @@
-import { component$, useStore, useWatch$ } from '@builder.io/qwik'
+import { component$, useStore, useResource$ } from '@builder.io/qwik'
 import { DocumentHead } from '@builder.io/qwik-city'
 
-interface AppStore {
-  count: number
-  delayCount: number
-}
-
 export default component$(() => {
-  const store = useStore<AppStore>({
-    count: 0,
-    delayCount: 0,
+  const github = useStore({
+    org: 'BuilderIO',
   })
-  console.log('Render: <App>')
-  useWatch$(({ track }) => {
-    track(() => store.count)
-    const id = setTimeout(() => (store.delayCount = store.count), 2000)
-    return () => clearTimeout(id)
+
+  const reposResource = useResource$<string[]>(({ track, cleanup }) => {
+    // We need a way to re-run fetching data whenever the `github.org` changes.
+    // Use `track` to trigger re-running of the this data fetching function.
+    // A good practice is to use `AbortController` to abort the fetching of data if
+    // new request comes in. We create a new `AbortController` and register a `cleanup`
+    // function which is called when this function re-runs.
+    // Fetch the data and return the promises.
   })
+
+  console.log('Render')
   return (
-    <>
-      <DisplayCount store={store} />
-      <DisplayDelayCount store={store} />
-      <button onClick$={() => store.count++}>+1</button>
-    </>
+    <div>
+      <span>
+        GitHub username:
+        <input
+          value={github.org}
+          onInput$={(ev) =>
+            (github.org = (ev.target as HTMLInputElement).value)
+          }
+        />
+      </span>
+      <div>
+        <Resource
+          value={reposResource}
+          onPending={() => <>Loading...</>}
+          onRejected={(error) => <>Error: {error.message}</>}
+          onResolved={(repos) => (
+            <ul>
+              {repos.map((repo) => (
+                <li>
+                  <a href={`https://github.com/${github.org}/${repo}`}>
+                    {repo}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          )}
+        />
+      </div>
+    </div>
   )
 })
 
-export const DisplayCount = component$((props: { store: AppStore }) => {
-  console.log('Render: <DisplayA>')
-  return <>{props.store.count}</>
-})
-
-export const DisplayDelayCount = component$((props: { store: AppStore }) => {
-  console.log('Render: <DisplayB>')
-  return <>{props.store.delayCount}</>
-})
+export async function getRepositories(
+  username: string,
+  controller?: AbortController
+): Promise<string[]> {
+  console.log('FETCH', `https://api.github.com/users/${username}/repos`)
+  const resp = await fetch(`https://api.github.com/users/${username}/repos`, {
+    signal: controller?.signal,
+  })
+  console.log('FETCH resolved')
+  const json = await resp.json()
+  return Array.isArray(json)
+    ? json.map((repo: { name: string }) => repo.name)
+    : Promise.reject(json)
+}
 
 export const head: DocumentHead = {
   title: 'Welcome to Qwik',
