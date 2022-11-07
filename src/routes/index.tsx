fe577c69122695f4eb3fee980632bb2933f280dd
JiaLiPassion <JiaLi.Passion@gmail.com>
2022-11-07 16:07:06 +0900

    module4-state-exercise-2

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,26 @@
 import { component$, useStore } from '@builder.io/qwik'
 import { DocumentHead } from '@builder.io/qwik-city'
 
+interface CountStore {
+  count: number
+}
+
 export default component$(() => {
-  const counter = useStore({ count: 0 })
+  const store = useStore<CountStore>({ count: 0 })
 
   return (
     <>
-      <div>Count: {counter.count}</div>
-      <button onClick$={() => counter.count++}>+1</button>
+      <button onClick$={() => store.count++}>+1</button>
+      <Display count={store.count} />
     </>
   )
+})
+
+interface DisplayProps {
+  count: number
+}
+export const Display = component$((props: DisplayProps) => {
+  return <div>The count is: {props.count}</div>
 })
 
 export const head: DocumentHead = {
