84849f69738109be898f0c9ac8589dba46d526d8
JiaLiPassion <JiaLi.Passion@gmail.com>
2022-11-07 14:54:59 +0900

    module4-state-exercise-1

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,72 +1,14 @@
-import {
-  component$,
-  useStore,
-  useStyles$,
-  useClientEffect$,
-} from '@builder.io/qwik'
+import { component$, useStore } from '@builder.io/qwik'
 import { DocumentHead } from '@builder.io/qwik-city'
-import styles from './clock.css'
-
-interface ClockStore {
-  hour: number
-  minute: number
-  second: number
-}
-export const Clock = component$(() => {
-  useStyles$(styles)
-
-  const store = useStore<ClockStore>({
-    hour: 0,
-    minute: 0,
-    second: 0,
-  })
-
-  useClientEffect$(() => {
-    updateClock(store)
-    const tmrId = setInterval(() => updateClock(store), 1000)
-    return () => clearInterval(tmrId)
-  })
-
-  return (
-    <div class="clock">
-      <div class="twelve"></div>
-      <div class="three"></div>
-      <div class="six"></div>
-      <div class="nine"></div>
-      <div class="hour" style={{ transform: `rotate(${store.hour}deg)` }}></div>
-      <div
-        class="minute"
-        style={{ transform: `rotate(${store.minute}deg)` }}
-      ></div>
-      <div
-        class="second"
-        style={{ transform: `rotate(${store.second}deg)` }}
-      ></div>
-    </div>
-  )
-})
-
-export function updateClock(store: ClockStore) {
-  const now = new Date()
-  store.second = now.getSeconds() * (360 / 60)
-  store.minute = now.getMinutes() * (360 / 60)
-  store.hour = now.getHours() * (360 / 12)
-}
 
 export default component$(() => {
+  const counter = { count: 0 }
+
   return (
-    <div>
-      <p>
-        This is an example of Lazy executing code on component when component
-        becomes visible.
-      </p>
-
-      <p style={{ height: '800px' }}>
-        ⬇️ <strong>Scroll down</strong> until the clock is in view.
-      </p>
-
-      <Clock />
-    </div>
+    <>
+      <div>Count: {counter.count}</div>
+      <button onClick$={() => counter.count++}>+1</button>
+    </>
   )
 })
 
