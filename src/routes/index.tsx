db01e87fbffcfe5d9e825ddeee10ff3ae4f7e746
JiaLiPassion <JiaLi.Passion@gmail.com>
2022-11-07 22:39:15 +0900

    module4-state-exercise-4

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,39 +1,41 @@
-import {
-  component$,
-  createContext,
-  useContextProvider,
-  useStore,
-  useContext,
-} from '@builder.io/qwik'
+import { component$, useStore, useWatch$ } from '@builder.io/qwik'
 import { DocumentHead } from '@builder.io/qwik-city'
 
-interface TodosStore {
-  items: string[]
+interface AppStore {
+  count: number
+  delayCount: number
 }
-export const TodosContext = createContext<TodosStore>('Todos')
 
 export default component$(() => {
-  useContextProvider(
-    TodosContext,
-    useStore<TodosStore>({
-      items: ['Learn Qwik', 'Build Qwik app', 'Profit'],
-    })
+  const store = useStore<AppStore>({
+    count: 0,
+    delayCount: 0,
+  })
+  console.log('Render: <App>')
+  useWatch$(({ track }) => {
+    // tracking `store.count`
+    // setup a timer to copy `count => delayCount` after 2 seconds.
+    return () => {
+      // cleanup code
+    }
+  })
+  return (
+    <>
+      <DisplayCount store={store} />
+      <DisplayDelayCount store={store} />
+      <button onClick$={() => store.count++}>+1</button>
+    </>
   )
-
-  return <Items />
 })
 
-export const Items = component$(() => {
-  // replace this with context retrieval.
-  const todos = useContext(TodosContext)
+export const DisplayCount = component$((props: { store: AppStore }) => {
+  console.log('Render: <DisplayA>')
+  return <>{props.store.count}</>
+})
 
-  return (
-    <ul>
-      {todos.items.map((item) => (
-        <li>{item}</li>
-      ))}
-    </ul>
-  )
+export const DisplayDelayCount = component$((props: { store: AppStore }) => {
+  console.log('Render: <DisplayB>')
+  return <>{props.store.delayCount}</>
 })
 
 export const head: DocumentHead = {
