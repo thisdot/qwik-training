7417386ae0609aaf5bb8e7a74458b7c972473cb3
JiaLiPassion <JiaLi.Passion@gmail.com>
2022-11-07 22:45:56 +0900

    module4-state-exercise-5-answer

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,4 @@
-import { component$, useStore, useResource$ } from '@builder.io/qwik'
+import { component$, useStore, useResource$, Resource } from '@builder.io/qwik'
 import { DocumentHead } from '@builder.io/qwik-city'
 
 export default component$(() => {
@@ -9,10 +9,16 @@ export default component$(() => {
   const reposResource = useResource$<string[]>(({ track, cleanup }) => {
     // We need a way to re-run fetching data whenever the `github.org` changes.
     // Use `track` to trigger re-running of the this data fetching function.
+    track(() => github.org)
+
     // A good practice is to use `AbortController` to abort the fetching of data if
     // new request comes in. We create a new `AbortController` and register a `cleanup`
     // function which is called when this function re-runs.
+    const controller = new AbortController()
+    cleanup(() => controller.abort())
+
     // Fetch the data and return the promises.
+    return getRepositories(github.org, controller)
   })
 
   console.log('Render')
