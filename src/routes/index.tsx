73909cf9cd0bc48a402a31a4959a7df6f2b4da02
JiaLiPassion <JiaLi.Passion@gmail.com>
2022-11-07 22:21:37 +0900

    module4-state-exercise-3-answer

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,6 +3,7 @@ import {
   createContext,
   useContextProvider,
   useStore,
+  useContext,
 } from '@builder.io/qwik'
 import { DocumentHead } from '@builder.io/qwik-city'
 
@@ -24,7 +25,8 @@ export default component$(() => {
 
 export const Items = component$(() => {
   // replace this with context retrieval.
-  const todos = { items: [] }
+  const todos = useContext(TodosContext)
+
   return (
     <ul>
       {todos.items.map((item) => (
