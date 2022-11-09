817e21fed0b3f02d03e71beeaffdede17039a1f9
JiaLiPassion <JiaLi.Passion@gmail.com>
2022-11-09 09:14:34 +0000

    module5-project-exercise-2

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,23 +2,26 @@ import type { DocumentHead } from '@builder.io/qwik-city'
 import { component$, useStore, Slot } from '@builder.io/qwik'
 
 export default component$(() => {
-  const store = useStore({ count: 0 })
-  console.log('Render: <App>')
+    console.log('Render: <App>');
   return (
-    <Panel>
-      Count: {store.count}. <button onClick$={() => store.count++}>+1</button>
-    </Panel>
-  )
+    <Collapsable>
+      <div q:slot="closed">▶ (collapsed summary)</div>
+      <div q:slot="open">
+        ▼<div> Content that should be displayed when the collapse component is open. </div>
+      </div>
+    </Collapsable>
+  );
 })
 
-export const Panel = component$(() => {
-  console.log('Render: <Panel>')
+export const Collapsable = component$(() => {
+  console.log('Render: <Collapsable>');
+  const store = useStore({ open: true });
   return (
-    <div style={{ border: '2px solid red;', padding: '1em' }}>
-      <Slot />
+    <div onClick$={() => (store.open = !store.open)}>
+      {store.open ? <Slot name="open" /> : `▶`}
+      {/* Instead, project content from the parent named "closed" here */}
     </div>
-  )
-})
+  );
 
 export const head: DocumentHead = {
   title: 'Welcome to Qwik',
