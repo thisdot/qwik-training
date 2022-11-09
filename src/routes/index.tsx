bb4ccfeb4f5f8903dce454a2dffea86bd7dea549
JiaLiPassion <JiaLi.Passion@gmail.com>
2022-11-09 09:13:48 +0000

    module5-project-exercise-1-answer

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,5 @@
 import type { DocumentHead } from '@builder.io/qwik-city'
-import { component$, useStore } from '@builder.io/qwik'
+import { component$, useStore, Slot } from '@builder.io/qwik'
 
 export default component$(() => {
   const store = useStore({ count: 0 })
@@ -15,9 +15,7 @@ export const Panel = component$(() => {
   console.log('Render: <Panel>')
   return (
     <div style={{ border: '2px solid red;', padding: '1em' }}>
-      Currently the <tt>&lt;Panel&gt;</tt> component controls the content here.
-      Replace this text with <tt>&lt;Slot&gt;</tt> element to see the content
-      projected from the <tt>&lt;App&gt;</tt>.
+      <Slot />
     </div>
   )
 })
