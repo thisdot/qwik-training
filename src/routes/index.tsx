a2d28ebdec4c219d7f30e323d0cebca40132d0a9
JiaLiPassion <JiaLi.Passion@gmail.com>
2022-11-09 09:18:24 +0000

    module5-project-exercise-3

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,28 +1,40 @@
 import type { DocumentHead } from '@builder.io/qwik-city'
-import { component$, useStore, Slot } from '@builder.io/qwik'
+import { component$, useStyles$, Slot } from '@builder.io/qwik'
 
 export default component$(() => {
-  console.log('Render: <App>')
   return (
-    <Collapsable>
-      <div q:slot="closed">▶ (collapsed summary)</div>
-      <div q:slot="open">
-        ▼
-        <div>
-          {' '}
-          Content that should be displayed when the collapse component is open.{' '}
-        </div>
-      </div>
-    </Collapsable>
+    <>
+      <Card>
+        <span q:slot="title">Qwik</span>
+        <span q:slot="body">
+          Qwik is a resumable framework for building instant web apps.
+        </span>
+      </Card>
+      <Card>
+        <span q:slot="title">Partytown</span>
+      </Card>
+      <Card>
+        <span q:slot="body">
+          Builder.io allows you to visually build on your tech stack Empower
+          your entire team to visually create and optimize high-speed
+          experiences on your sites and apps. Provide whole-team autonomy with a
+          platform that is developer approved.
+        </span>
+      </Card>
+    </>
   )
 })
 
-export const Collapsable = component$(() => {
-  console.log('Render: <Collapsable>')
-  const store = useStore({ open: true })
+export const Card = component$(() => {
+  useStyles$(CSS)
   return (
-    <div onClick$={() => (store.open = !store.open)}>
-      {store.open ? <Slot name="open" /> : <Slot name="closed" />}
+    <div class="card">
+      <div class="title">
+        <Slot name="title"></Slot>
+      </div>
+      <div class="body">
+        <Slot name="body"></Slot>
+      </div>
     </div>
   )
 })
@@ -30,3 +42,36 @@ export const Collapsable = component$(() => {
 export const head: DocumentHead = {
   title: 'Welcome to Qwik',
 }
+
+export const CSS = `
+.card {
+  border-radius: 5px;
+  vertical-align: top;
+  display: inline-block;
+  border: 1px solid grey;
+  width: 200px;
+  margin: .5em;
+}
+
+.title {
+  background-color: lightgray;
+  padding: 0.5em;
+  border-bottom: 1px solid black;
+}
+
+/* Add the right CSS selector here to make a fallback for slot named "title" */
+q\\:title {
+  content: 'Fallback title';
+  color: red;
+}
+
+.body {
+  padding: 0.5em;
+}
+
+/* Add the right CSS selector here to make a fallback for slot named "body" */
+q\\:body {
+  content: 'Fallback body';
+  color: orange;
+}
+`
