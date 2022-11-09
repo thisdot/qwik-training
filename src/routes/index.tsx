5960b009389786f2acca79d211daa5c0954c6dde
JiaLiPassion <JiaLi.Passion@gmail.com>
2022-11-09 09:13:04 +0000

    module5-project-exercise-1

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,25 @@
 import type { DocumentHead } from '@builder.io/qwik-city'
-import { component$ } from '@builder.io/qwik'
+import { component$, useStore } from '@builder.io/qwik'
 
 export default component$(() => {
-  return <button onClick$={() => alert('Hello World!')}>Click Me</button>
+  const store = useStore({ count: 0 })
+  console.log('Render: <App>')
+  return (
+    <Panel>
+      Count: {store.count}. <button onClick$={() => store.count++}>+1</button>
+    </Panel>
+  )
+})
+
+export const Panel = component$(() => {
+  console.log('Render: <Panel>')
+  return (
+    <div style={{ border: '2px solid red;', padding: '1em' }}>
+      Currently the <tt>&lt;Panel&gt;</tt> component controls the content here.
+      Replace this text with <tt>&lt;Slot&gt;</tt> element to see the content
+      projected from the <tt>&lt;App&gt;</tt>.
+    </div>
+  )
 })
 
 export const head: DocumentHead = {
