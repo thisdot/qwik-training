1ad0a6c9cba504db336c3a853c728b4b68b57e7b
JiaLiPassion <JiaLi.Passion@gmail.com>
2023-01-04 01:46:08 +0000

    Update global event listener

--- src/routes/exercises/module3/exercise-1/exercise.tsx
+++ src/routes/exercises/module3/exercise-1/exercise.tsx
@@ -1,17 +1,19 @@
-import { component$, useStore } from '@builder.io/qwik'
+import { component$, useStore, useOnDocument, $ } from '@builder.io/qwik'
 
 export default component$(() => {
   const store = useStore({ x: 0, y: 0 })
+  useOnDocument(
+    'mousemove',
+    $((event: Event) => {
+      store.x = (event as MouseEvent).clientX
+      store.y = (event as MouseEvent).clientY
+    })
+  )
   /**
    * Exercise 3-1: Make mouseMove a global event listener
    */
   return (
-    <div
-      document:onMouseMove$={(event) => {
-        store.x = event.clientX
-        store.y = event.clientY
-      }}
-    >
+    <div>
       Your mouse location is ({store.x}, {store.y}).
     </div>
   )
