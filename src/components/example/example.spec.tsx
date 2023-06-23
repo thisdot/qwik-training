669ba10930c9ef85ef06459832cda300c43ead70
Jan Kaiser <honza.kaiser@gmail.com>
2023-06-23 13:23:07 +0200

    feat: exend test exercises

--- src/components/example/example.spec.tsx
+++ src/components/example/example.spec.tsx
@@ -34,3 +34,24 @@ test(`[ExampleTest Component]: Click counter +1`, async () => {
   const spanAfter = screen.querySelector('span') as HTMLDivElement
   // click count button and check the count result
 })
+
+/**
+ * Exercise 7-3: Unit test
+ */
+test(`[ExampleTest Component]: Click counter +1`, async () => {
+  const { screen, render, userEvent } = await createDOM()
+  await render(<ExampleTest flag={true} />)
+
+  expect(screen.outerHTML).toContain('Count:0')
+
+  const spanBefore = screen.querySelector('span') as HTMLDivElement
+
+  const input = screen.querySelector('.input-counter') as HTMLInputElement
+
+  // trigger input event with value 50
+
+  expect(spanBefore.innerHTML).toEqual('Count:50')
+
+  const spanAfter = screen.querySelector('span') as HTMLDivElement
+  // click count button and check the count result
+})
--- src/components/example/example.tsx
+++ src/components/example/example.tsx
@@ -1,17 +1,27 @@
-import { component$, useStore } from '@builder.io/qwik';
+import { $, component$, useStore } from '@builder.io/qwik'
 
 export const ExampleTest = component$((props: { flag: boolean }) => {
   const state = useStore({
     counter: 0,
-  });
+  })
+
+  const handleInput$ = $((e: any) => {
+    const value = parseInt(e.target.value)
+    if (!isNaN(value)) state.counter = value
+  })
 
   return (
     <>
       <span>Count:{state.counter}</span>
       <div class="icon">Flag: {props.flag ? '⭐' : '💣'}</div>
+      <input
+        class="input-counter"
+        value={state.counter}
+        onInput$={handleInput$}
+      />
       <button class="btn-counter" onClick$={() => state.counter++}>
         Increment counter
       </button>
     </>
-  );
-});
+  )
+})
